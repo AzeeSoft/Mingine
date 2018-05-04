66e5bed550b7a1ce79d0b7e0ff06a25126e0182b
Azee <aziztitu1996@gmail.com>
2018-05-04 18:06:07 -0500

    Merge branch 'azee' into master-azeesoft

--- Mingine/assets/scripts/exampleProjects/states.lua
+++ Mingine/assets/scripts/exampleProjects/states.lua
@@ -7,12 +7,7 @@ SCREEN_HEIGHT = 768
 MAX_ACCELERATION = 0.5 -- how fast can the agent change direction and speed?
 
 CHASE_START_DISTANCE = 200
-CHASE_END_DISTANCE = 250
-
-gameState = 0
-
-timer = 0
-timeLeft = 15000
+CHASE_END_DISTANCE = 500
 
 --------------------------------------------
 -- agent state behaviors
@@ -81,90 +76,11 @@ function ChaseUpdate(agent)
     --transitions
     if GetDistanceToMouse(agent) > CHASE_END_DISTANCE then 
         EnterState(agent, wanderState)
-    elseif GetDistanceToMouse(agent) < 10 then
-        gameState = -1;
     end
 end
 
 function ChaseExit(agent)
     --do nothing
-end
-
-
-
-function PacingEnter(agent)
-    if not agent.dirX then
-        agent.dirX = 1
-    end
-
-    agent.startX = agent.x
-end
-
-function PacingUpdate(agent)
-    agent.velocity.y = 0
-
-    agent.velocity.x = 5 * agent.dirX
-    UpdateEntity(agent)
-
-    local yDir = 1;
-    if agent.y > SCREEN_HEIGHT/2 then
-        yDir = -1
-    end
-
-    TurnTo(agent, CreateVector(0, yDir))
-
-    if agent.x < 0 or agent.x > SCREEN_WIDTH then
-        agent.dirX = agent.dirX * -1;
-    end
-
-    if math.abs(agent.x - agent.startX) > 100 then
-        EnterState(agent, passState)
-    end
-
-    if GetDistanceToMouse(agent) < 32 then
-        gameState = -1;
-    end
-end
-
-function PacingExit(agent)
-
-end
-
-
-function PassEnter(agent)
-    agent.velocity.x = 0
-
-    agent.readyToSwitch = false
-    agent.dirY = 1
-    if agent.y > SCREEN_HEIGHT/2 then
-        agent.dirY = -1;
-    end
-end
-
-function PassUpdate(agent)
-    agent.velocity.y = 20 * agent.dirY
-    UpdateEntity(agent)
-
-    local yDir = 1;
-    if agent.y > SCREEN_HEIGHT/2 then
-        yDir = -1
-    end
-
-    TurnTo(agent, CreateVector(0, yDir))
-
-    if agent.y >= SCREEN_HEIGHT-32 or agent.y <= 32 then
-        agent.dirY = agent.dirY * -1
-
-        EnterState(agent, pacingState)
-    end
-
-    if GetDistanceToMouse(agent) < 32 then
-        gameState = -1;
-    end
-end
-
-function PassExit(agent)
-
 end
 
 --------------------------------------------
@@ -183,17 +99,6 @@ chaseState.Enter = ChaseEnter
 chaseState.Update = ChaseUpdate
 chaseState.Exit = ChaseExit
 
-pacingState = {}
-pacingState.name = "Pacing"
-pacingState.Enter = PacingEnter
-pacingState.Update = PacingUpdate
-pacingState.Exit = PacingExit
-
-passState = {}
-passState.name = "Pass"
-passState.Enter = PassEnter
-passState.Update = PassUpdate
-passState.Exit = PassExit
 --------------------------------------------
 -- mingine hooks
 --------------------------------------------
@@ -206,39 +111,15 @@ function Start()
     alertSfx = LoadSound("sfx/happy.wav")
     
     local image = LoadImage("images/arrow.png")
-
-    agents = {}
-
-    for i=1, 10 do 
-        agent = CreateEntity(image, SCREEN_WIDTH/2, SCREEN_HEIGHT/2, 32, 16)
-        agent.maxSpeed = 10
-        agent.wanderAngle = math.random(0, 360) 
-        
-        CreateStateMachine(agent, wanderState)
-
-        agents[#agents+1] = agent
-    end
-
-    agent2 = CreateEntity(image, SCREEN_WIDTH / 2, SCREEN_HEIGHT - 32, 32, 16)
-    agent2.maxSpeed = 40
-
-    CreateStateMachine(agent2, pacingState)
+    agent = CreateEntity(image, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 32, 16)
+    agent.maxSpeed = 10
+    agent.wanderAngle = 0
+    
+    CreateStateMachine(agent, wanderState)
 end
 
 function Update()
-    if gameState == 0 then
-        timeLeft = timeLeft - GetFrameTime()
-        if timeLeft <= 0 then
-            timeLeft = 0
-            gameState = 1
-        else 
-            for i=1, #agents do
-                UpdateStateMachine(agents[i])
-            end
-            
-            UpdateStateMachine(agent2)
-        end
-    end
+   UpdateStateMachine(agent)
 end
 
 function Draw()
@@ -246,21 +127,10 @@ function Draw()
             
     SetDrawColor(255, 0, 255, 255)
               
-    if gameState == 0 then
-        for i=1, #agents do
-            DrawEntity(agents[i])
-        end
-        DrawEntity(agent2)
+    DrawEntity(agent)
+    --line over agent represents direction of acceleration
+    local accDirX, accDirY = Mad(agent, agent.acceleration, 32)
+    DrawLine(agent.x, agent.y, accDirX, accDirY) 
 
-        --line over agent represents direction of acceleration
-        local accDirX, accDirY = Mad(agent, agent.acceleration, 32)
-        DrawLine(agent.x, agent.y, accDirX, accDirY) 
-
-        DrawText("State: " .. agent.stateMachine.currentState.name, 8, 9, font, 255, 255, 255)
-        DrawText("Time Left: " .. timeLeft/1000, 8, 40, font, 255, 255, 255)
-    elseif gameState == -1 then
-        DrawText("You Lost!!", SCREEN_WIDTH/2 - 50, SCREEN_HEIGHT/2, font, 255, 255, 255)
-    else
-        DrawText("You Won!!", SCREEN_WIDTH/2 - 50, SCREEN_HEIGHT/2, font, 255, 255, 255)
-    end
+    DrawText("State: " .. agent.stateMachine.currentState.name, 8, 9, font, 255, 255, 255)
 end
